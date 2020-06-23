41f193ca82e5ae6516551d35562ec00789e6bcb4
Cassandra Bailey <cassandra.bailey@connexta.com>
2020-06-23 09:23:08 -0600

    DDF-UI-248 use other polyfill and cleanup

--- ui/packages/catalog-ui-search/src/main/webapp/js/EventSourceUtil.js
+++ ui/packages/catalog-ui-search/src/main/webapp/js/EventSourceUtil.js
@@ -1,11 +1,30 @@
-import eventTarget from './EventTarget'
+var EventSource = require('eventsource')
+
+const ORIGIN_HEADER = 'https://localhost:8993/'
+const REQUEST_HEADER = 'XMLHttpRequest'
+const HEADERS = {
+  Origin: ORIGIN_HEADER,
+  'X-Requested-With': REQUEST_HEADER,
+}
+
+var sources = []
 
 module.exports = {
   createEventListener(type, handlers) {
     const { onMessage } = handlers
-
-    eventTarget.addEventListener(type, event => {
-      onMessage(event)
-    })
+    if (sources.length != 0) {
+      sources[0].addEventListener(type, event => {
+        onMessage(event)
+      })
+    } else {
+      var source = new EventSource('./internal/events', {
+        withCredentials: true,
+        headers: HEADERS,
+      })
+      source.addEventListener(type, event => {
+        onMessage(event)
+      })
+      sources.push(source)
+    }
   },
 }
